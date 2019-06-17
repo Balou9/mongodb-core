3a2bea1a24c4886c2e0bcd8ec74f6222a167b98b
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-17 18:15:04 +0200

    making da connection

--- deno_lib/connection/connect.ts
+++ deno_lib/connection/connect.ts
@@ -20,55 +20,54 @@ import { defaultAuthProviders } from "./../auth/defaultAuthProviders.ts"
 import {MAX_SUPPORTED_WIRE_VERSION, MAX_SUPPORTED_SERVER_VERSION,
 MIN_SUPPORTED_WIRE_VERSION, MIN_SUPPORTED_SERVER_VERSION} from "./../wireprotocol/constants.ts"
 
-let AUTH_PROVIDERS;
+export interface ConnectOptions {
+  family?: number;
+  bson?: unknown;
+}
 
-function connect(options, callback) {
-  if (AUTH_PROVIDERS == null) {
+let AUTH_PROVIDERS: unknown;
+
+function connect(options: ConnectOptions, callback: (err?: Error, socket?: unknown) => void): void {
+  if (!AUTH_PROVIDERS) {
     AUTH_PROVIDERS = defaultAuthProviders(options.bson);
   }
 
   if (options.family !== void 0) {
-    makeConnection(options.family, options, (err, socket) => {
+    return makeConnection(options.family, options, (err: Error, socket: unknown): void => {
       if (err) {
-        callback(err, socket); // in the error case, `socket` is the originating error event name
-        return;
+        return callback(err, socket); // in the error case, `socket` is the originating error event name
       }
 
       performInitialHandshake(new Connection(socket, options), options, callback);
     });
-
-    return;
   }
 
-  return makeConnection(6, options, (err, ipv6Socket) => {
+  return makeConnection(6, options, (err: Error, ipv6Socket: unknown): void => {
     if (err) {
-      makeConnection(4, options, (err, ipv4Socket) => {
+      return makeConnection(4, options, (err: Error, ipv4Socket: unknown): void => {
         if (err) {
-          callback(err, ipv4Socket); // in the error case, `ipv4Socket` is the originating error event name
-          return;
+          return callback(err, ipv4Socket); // in the error case, `ipv4Socket` is the originating error event name
         }
 
         performInitialHandshake(new Connection(ipv4Socket, options), options, callback);
       });
-
-      return;
     }
 
     performInitialHandshake(new Connection(ipv6Socket, options), options, callback);
   });
 }
 
-function getSaslSupportedMechs(options) {
+function getSaslSupportedMechs(options?: {[key:string]: any}): { saslSupportedMechs?: string} {
   if (!(options && options.credentials)) {
     return {};
   }
 
-  const credentials = options.credentials;
+  // const credentials: {[key:string]: any} = options.credentials;
 
   // TODO: revisit whether or not items like `options.user` and `options.dbName` should be checked here
-  const authMechanism = credentials.mechanism;
-  const authSource = credentials.source || options.dbName || 'admin';
-  const user = credentials.username || options.user;
+  const authMechanism: any = options.credentials.mechanism;
+  const authSource: string = options.credentials.source || options.dbName || 'admin';
+  const user: string = options.credentials.username || options.user;
 
   if (typeof authMechanism === 'string' && authMechanism.toUpperCase() !== 'DEFAULT') {
     return {};
@@ -81,12 +80,12 @@ function getSaslSupportedMechs(options) {
   return { saslSupportedMechs: `${authSource}.${user}` };
 }
 
-function checkSupportedServer(ismaster, options) {
-  const serverVersionHighEnough =
+function checkSupportedServer(ismaster: {[key:string]: any}, options: {[key:string]: any}): MongoError {
+  const serverVersionHighEnough: boolean =
     ismaster &&
     typeof ismaster.maxWireVersion === 'number' &&
     ismaster.maxWireVersion >= MIN_SUPPORTED_WIRE_VERSION;
-  const serverVersionLowEnough =
+  const serverVersionLowEnough: boolean =
     ismaster &&
     typeof ismaster.minWireVersion === 'number' &&
     ismaster.minWireVersion <= MAX_SUPPORTED_WIRE_VERSION;
@@ -96,33 +95,33 @@ function checkSupportedServer(ismaster, options) {
       return null;
     }
 
-    const message = `Server at ${options.host}:${options.port} reports minimum wire version ${
+    const message: string = `Server at ${options.host}:${options.port} reports minimum wire version ${
       ismaster.minWireVersion
     }, but this version of the Node.js Driver requires at most ${MAX_SUPPORTED_WIRE_VERSION} (MongoDB ${MAX_SUPPORTED_SERVER_VERSION})`;
     return new MongoError(message);
   }
 
-  const message = `Server at ${options.host}:${
+  const message: string = `Server at ${options.host}:${
     options.port
   } reports maximum wire version ${ismaster.maxWireVersion ||
     0}, but this version of the Node.js Driver requires at least ${MIN_SUPPORTED_WIRE_VERSION} (MongoDB ${MIN_SUPPORTED_SERVER_VERSION})`;
   return new MongoError(message);
 }
 
-function performInitialHandshake(conn, options, _callback) {
-  const callback = function(err, ret) {
+function performInitialHandshake(conn: Connection, options: {[key:string]: any}, callback: (err?: Error, socket?: unknown) => void): void {
+  function _callback(err?: Error, rtn?: unknown): void {
     if (err && conn) {
       conn.destroy();
     }
-    _callback(err, ret);
+    callback(err, rtn);
   };
 
-  let compressors = [];
+  let compressors: unknown[] = [];
   if (options.compression && options.compression.compressors) {
     compressors = options.compression.compressors;
   }
 
-  const handshakeDoc = Object.assign(
+  const handshakeDoc: {[key:string]: any} = Object.assign(
     {
       ismaster: true,
       client: createClientInfo(options),
@@ -131,27 +130,25 @@ function performInitialHandshake(conn, options, _callback) {
     getSaslSupportedMechs(options)
   );
 
-  const start = new Date().getTime();
-  runCommand(conn, 'admin.$cmd', handshakeDoc, options, (err, ismaster) => {
+  const start: number = new Date().getTime();
+  runCommand(conn, 'admin.$cmd', handshakeDoc, options, (err: Error, ismaster: {[key:string]: any}): void => {
     if (err) {
-      callback(err, null);
-      return;
+      return _callback(err, null);
     }
 
     if (ismaster.ok === 0) {
-      callback(new MongoError(ismaster), null);
-      return;
+      return _callback(new MongoError(ismaster), null);
     }
 
-    const supportedServerErr = checkSupportedServer(ismaster, options);
+    const supportedServerErr: MongoError = checkSupportedServer(ismaster, options);
+    
     if (supportedServerErr) {
-      callback(supportedServerErr, null);
-      return;
+      return   _callback(supportedServerErr, null);
     }
 
     // resolve compression
     if (ismaster.compression) {
-      const agreedCompressors = compressors.filter(
+      const agreedCompressors: unknown[] = compressors.filter(
         compressor => ismaster.compression.indexOf(compressor) !== -1
       );
 
@@ -170,18 +167,18 @@ function performInitialHandshake(conn, options, _callback) {
     conn.ismaster = ismaster;
     conn.lastIsMasterMS = new Date().getTime() - start;
 
-    const credentials = options.credentials;
+    const credentials: Credentials = options.credentials;
+    
     if (!ismaster.arbiterOnly && credentials) {
       credentials.resolveAuthMechanism(ismaster);
-      authenticate(conn, credentials, callback);
-      return;
+      return authenticate(conn, credentials, _callback);
     }
 
-    callback(null, conn);
+    _callback(null, conn);
   });
 }
 
-const LEGAL_SSL_SOCKET_OPTIONS = [
+const LEGAL_SSL_SOCKET_OPTIONS: string[] = [
   'pfx',
   'key',
   'passphrase',
@@ -200,13 +197,14 @@ const LEGAL_SSL_SOCKET_OPTIONS = [
   'rejectUnauthorized'
 ];
 
-function parseConnectOptions(family, options) {
-  const host = typeof options.host === 'string' ? options.host : 'localhost';
+function parseConnectOptions(family: number, options: {[key:string]: any}): {[key:string]: any} {
+  const host: string = typeof options.host === 'string' ? options.host : 'localhost';
+  
   if (host.indexOf('/') !== -1) {
     return { path: host };
   }
 
-  const result = {
+  const result: {[key:string]: any} = {
     family,
     host,
     port: typeof options.port === 'number' ? options.port : 27017,
@@ -216,8 +214,8 @@ function parseConnectOptions(family, options) {
   return result;
 }
 
-function parseSslOptions(family, options) {
-  const result = parseConnectOptions(family, options);
+function parseSslOptions(family: number, options: {[key:string]: any}): {[key:string]: any} {
+  const result: {[key:string]: any} = parseConnectOptions(family, options);
 
   // Merge in valid SSL options
   for (const name in options) {
@@ -227,25 +225,23 @@ function parseSslOptions(family, options) {
   }
 
   // Override checkServerIdentity behavior
-  if (options.checkServerIdentity === false) {
+  if (!options.checkServerIdentity) {
     // Skip the identiy check by retuning undefined as per node documents
     // https://nodejs.org/api/tls.html#tls_tls_connect_options_callback
-    result.checkServerIdentity = function() {
-      return undefined;
-    };
+    result.checkServerIdentity = (..._: any[]): void => undefined;
   } else if (typeof options.checkServerIdentity === 'function') {
     result.checkServerIdentity = options.checkServerIdentity;
   }
 
   // Set default sni servername to be the same as host
-  if (result.servername == null) {
+  if (!result.servername) {
     result.servername = result.host;
   }
 
   return result;
 }
 
-function makeConnection(family, options, _callback) {
+function makeConnection(family: number, options: {[key:string]: any}, callback:(err?: Error, socket?: unknown) => void): void {
   const useSsl = typeof options.ssl === 'boolean' ? options.ssl : false;
   const keepAlive = typeof options.keepAlive === 'boolean' ? options.keepAlive : true;
   let keepAliveInitialDelay =
@@ -261,12 +257,13 @@ function makeConnection(family, options, _callback) {
     keepAliveInitialDelay = Math.round(socketTimeout / 2);
   }
 
-  let socket;
-  const callback = function(err, ret) {
+  let socket: unknown;
+  function _callback(err: Error, rtn: unknown): void {
     if (err && socket) {
       socket.destroy();
     }
-    _callback(err, ret);
+    
+    callback(err, rtn);
   };
 
   try {
@@ -279,30 +276,31 @@ function makeConnection(family, options, _callback) {
       socket = net.createConnection(parseConnectOptions(family, options));
     }
   } catch (err) {
-    return callback(err);
+    return _callback(err);
   }
 
   socket.setKeepAlive(keepAlive, keepAliveInitialDelay);
   socket.setTimeout(connectionTimeout);
   socket.setNoDelay(noDelay);
 
-  const errorEvents = ['error', 'close', 'timeout', 'parseError', 'connect'];
-  function errorHandler(eventName) {
-    return err => {
-      errorEvents.forEach(event => socket.removeAllListeners(event));
+  const errorEvents: string[] = ['error', 'close', 'timeout', 'parseError', 'connect'];
+  
+  function errorHandler(eventName: string): (err: Error) => void {
+    return (err: Error): void => {
+      errorEvents.forEach((event: string) => socket.removeAllListeners(event));
       socket.removeListener('connect', connectHandler);
-      callback(connectionFailureError(eventName, err), eventName);
+      _callback(connectionFailureError(eventName, err), eventName);
     };
   }
 
-  function connectHandler() {
+  function connectHandler(): void {
     errorEvents.forEach(event => socket.removeAllListeners(event));
     if (socket.authorizationError && rejectUnauthorized) {
-      return callback(socket.authorizationError);
+      return _callback(socket.authorizationError);
     }
 
     socket.setTimeout(socketTimeout);
-    callback(null, socket);
+    _callback(null, socket);
   }
 
   socket.once('error', errorHandler('error'));
