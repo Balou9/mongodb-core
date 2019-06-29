a407c2bd30b40084908d801b64cb1b569e74a81a
Noah Anabiik Schwarz <noah.anabiik.schwarz@gmail.com>
2019-06-29 13:32:50 +0200

    wire mod

---
+++ deno_lib/wireprotocol/mod.ts
@@ -0,0 +1,26 @@
+import { writeCommand} from "./write_command.ts"
+import { Callback} from "./../utils.ts"
+
+export {killCursors} from "./kill_cursors.ts"
+export {getMore} from "./get_more.ts"
+export {query} from "./query.ts"
+export {command} from "./command.ts"
+
+
+export function  insert(server: unknown, ns: string, ops: unknown, options: {[key:string]: any}, callback: Callback) {
+    writeCommand(server, 'insert', 'documents', ns, ops, options, callback);
+  }
+  
+export function  update(server: unknown, ns: string, ops: unknown,options : {[key:string]: any}, callback: Callback) {
+    writeCommand(server, 'update', 'updates', ns, ops, options, callback);
+  }
+  
+export  function remove(server: unknown, ns: string, ops: unknown, options: {[key:string]: any}, callback: Callback) {
+    writeCommand(server, 'delete', 'deletes', ns, ops, options, callback);
+  }
+  
+// export   killCursors: require('./kill_cursors'),
+//   getMore: require('./get_more'),
+//   query: require('./query'),
+//   command: require('./command')
+// };
